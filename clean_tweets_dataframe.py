8e5647549ebb54befd82156c1af6e87f9766105a
Yosef Alemneh <yosefworku67@gmail.com>
2021-06-21 20:26:43 +0300

    fixed clean_tweets_dataframe.py

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,3 +1,4 @@
+import pandas as pd
 class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
@@ -24,9 +25,7 @@ class Clean_Tweets:
         """
         convert column to datetime
         """
-        ----
-        
-        ----
+        df['created_at'] = pd.to_datetime(df['created_at'], format='%y%m%d')
         
         df = df[df['created_at'] >= '2020-12-31' ]
         
@@ -37,10 +36,7 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd.----
-        
-        ----
-        ----
+        df[['polarity','subjectivity', 'retweet_count','favourite_count']] = pd.to_numeric(df['polarity', 'subjectivity', 'retweet_count', 'favorite_count'])
         
         return df
     
@@ -49,6 +45,6 @@ class Clean_Tweets:
         remove non english tweets from lang
         """
         
-        df = ----
+        df = df[df["lang"] == "en"]
         
         return df
