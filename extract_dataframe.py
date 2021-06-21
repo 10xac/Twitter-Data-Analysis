6094912af2e37e9980b7488ad2c81a557dbd7ed3
Adeyemo Oluwatosin Maryam <64064011+a3o1m2@users.noreply.github.com>
2021-06-21 20:55:15 +0100

    Update extract_dataframe.py

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -35,51 +35,57 @@ class TweetDfExtractor:
 
     # an example function
     def find_statuses_count(self)->list:
-        statuses_count 
+        statuses_count = self.df['user.statuses_count'].to_list()
+        return statuses_count
         
     def find_full_text(self)->list:
-        text = 
-       
+        text = self.df['text'].to_list()
+        return text
     
-    def find_sentiments(self, text)->list:
-        
-        return polarity, self.subjectivity
+    def find_sentiments(self, text_list)->list:
+        polarity = [TextBlob(text).sentiment.polarity for text in text_list]
+        subjectivity = [TextBlob(text).sentiment.subjectivity for text in text_list]
+        return polarity, subjectivity
 
     def find_created_time(self)->list:
-       
+        created_at = self.df['created_at'].to_list()
         return created_at
 
     def find_source(self)->list:
-        source = 
-
+        source = self.df['source'].to_list()
         return source
 
     def find_screen_name(self)->list:
-        screen_name = 
+        screen_name = self.df['user.screen_name'].to_list()
+        return screen_name
 
     def find_followers_count(self)->list:
-        followers_count = 
+        followers_count = self.df['user.followers_count'].to_list()
+        return followers_count
 
     def find_friends_count(self)->list:
-        friends_count = 
+        friends_count = self.df['user.friends_count'].to_list()
+        return friends_count
 
     def is_sensitive(self)->list:
         try:
-            is_sensitive = [x['possibly_sensitive'] for x in self.tweets_list]
+            is_sensitive = [self.df['possibly_sensitive'] for x in self.tweets_list]
         except KeyError:
             is_sensitive = None
 
         return is_sensitive
 
     def find_favourite_count(self)->list:
-        
+        return self.df['user.favourites_count'].to_list()
     
     def find_retweet_count(self)->list:
-        retweet_count = 
+        retweet_count = self.df['retweet_count'].to_list()
+        return retweet_count
 
     def find_hashtags(self)->list:
-        hashtags =
-
+        hashtags = self.df['entities.hashtags'].to_list()
+        return hashtags
+    
     def find_mentions(self)->list:
         mentions = 
 
