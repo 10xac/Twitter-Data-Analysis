1fd26b9343795ece09dc8b710b282f0080db0165
katenjoki <62309540+katenjoki@users.noreply.github.com>
2021-06-21 20:42:41 +0300

    Update clean_tweets_dataframe.py

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -21,7 +21,7 @@ class Clean_Tweets:
         drop duplicate rows
         """
         
-        ---
+       df = df.drop_duplicates(subset=None,inplace=True)
         
         return df
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
