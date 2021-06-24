8e9e5eb165fee356f903ca338ffafbeb637b5b92
bina3c <48756392+Bina-man@users.noreply.github.com>
2021-06-24 22:18:32 +0300

    Finialized the unit test

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,9 +1,6 @@
 import pandas as pd
-<<<<<<< HEAD
-=======
 
 
->>>>>>> make_unittest
 class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
@@ -28,11 +25,7 @@ class Clean_Tweets:
         """
         drop duplicate rows
         """
-<<<<<<< HEAD
-        df = df.drop_duplicates()
-=======
         df.drop_duplicates(inplace=True)
->>>>>>> make_unittest
 
         return df
 
@@ -40,14 +33,10 @@ class Clean_Tweets:
         """
         convert column to datetime
         """
-<<<<<<< HEAD
-        df = pd.to_datetime(df['created_at'], errors='coerce')
-=======
 
         df['created_at'] = pd.to_datetime(
             df['created_at'])
 
->>>>>>> make_unittest
         return df
 
     def convert_to_numbers(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -55,16 +44,11 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-<<<<<<< HEAD
-        df['polarity', 'subjectivity', 'retweet_count', 'favorite_count'] = pd.to_numeric(df['polarity', 'subjectivity',
-                                                                                             'retweet_count', 'favorite_count'])
-=======
         df['polarity'] = pd.to_numeric(df["polarity"])
         df["subjectivity"] = pd.to_numeric(df["subjectivity"])
         df["retweet_count"] = pd.to_numeric(df["retweet_count"])
         df["favorite_count"] = pd.to_numeric(df["favorite_count"])
         df["friends_count "] = pd.to_numeric(df["polarity"])
->>>>>>> make_unittest
 
         return df
 
@@ -72,11 +56,7 @@ class Clean_Tweets:
         """
         remove non english tweets from lang
         """
-<<<<<<< HEAD
-        df = df[df["lang"] == "en"]
-=======
 
         df = df.drop(df[df['lang'] != 'en'].index)
 
         return df
->>>>>>> make_unittest
--- extract_dataframe.py
+++ extract_dataframe.py
@@ -30,123 +30,6 @@ class TweetDfExtractor:
     """
 
     def __init__(self, tweets_list):
-<<<<<<< HEAD
-        
-        self.tweets_list = tweets_list     
-
-
-    # an example function
-    def find_statuses_count(self)->list:
-        
-        statuses_count = []
-        for i in self.tweets_list:
-            statuses_count.append(i['user']['statuses_count'])
-        return statuses_count 
-        
-    def find_full_text(self)->list:
-        text=[]
-        for i in self.tweets_list:
-            text.append(i['text'])
-#         assert_equal.__self__.maxDiff = None    
-        return text 
-#         pass
-    def find_sentiments(self, text)->list:
-            polarity = []
-            subjectivity = []
-
-            for each in text:
-                if (each):
-                    result = TextBlob(str(each)).sentiment
-                    polarity.append(result.polarity)
-                    subjectivity.append(result.subjectivity)
-            
-            return polarity, subjectivity
-
-    def is_sensitive(self)->list:
-        isSensitive = []
-        for sensitive in self.tweets_list:
-            try:
-                is_sensitive = sensitive['possibly_sensitive']
-            except KeyError:
-                is_sensitive = None
-            isSensitive += [is_sensitive]
-        return isSensitive
-
-
-    def find_lang(self)->list:
-        lang = []
-        for i in self.tweets_list:
-            lang.append(i['lang'])
-        return lang
-
-    def find_created_time(self)->list:
-        created_at= []
-        for i in self.tweets_list:
-            created_at.append(i['created_at'])
-       
-        return created_at
-
-    def find_source(self)->list:
-        source = []
-        for i in self.tweets_list:
-            source.append(i['source'])
-
-        return source
-
-    def find_screen_name(self)->list:
-        screen_name = []
-        for i in self.tweets_list:
-            screen_name.append(i['user']['screen_name'])
-        return screen_name    
-
-    def find_followers_count(self)->list:
-        followers_count = []
-        for i in self.tweets_list:
-            followers_count.append(i['user']['followers_count'])
-        return followers_count
-
-    def find_friends_count(self)->list:
-        friends_count = []
-        for i in self.tweets_list:
-            friends_count.append(i['user']['friends_count'])
-        return friends_count    
-
-    
-
-    def find_favourite_count(self)->list:
-        favourite_count=[]
-        for i in self.tweets_list:
-            favourite_count.append(i['user']['favourites_count'])
-        return favourite_count    
-            
-    def find_retweet_count(self)->list:
-        retweet_count = []
-        for i in self.tweets_list:
-            retweet_count.append(i['retweet_count'])
-        return retweet_count
-
-    def find_hashtags(self)->list:
-        hashtags = []
-        for i in self.tweets_list:
-            hashtags.append(i['entities']['hashtags'])
-        return hashtags    
-
-    def find_mentions(self)->list:
-        try:
-           mentions= self.tweets_list['location']
-        except TypeError:
-            mentions = ''
-        
-        return mentions
-    
-     
-    def find_location(self)->list:
-        try:
-            location = self.tweets_list['user']['location']
-        except TypeError:
-            location = ''
-        
-=======
 
         self.tweets_list = tweets_list
 
@@ -239,21 +122,13 @@ class TweetDfExtractor:
         location = [tw.get('user', {}).get('location', None)
                     for tw in self.tweets_list]
 
->>>>>>> make_unittest
         return location
-    
 
-<<<<<<< HEAD
-    
-        
-    def get_tweet_df(self, save=False)->pd.DataFrame:
-=======
     def find_lang(self) -> list:
         lang = [tw.get('lang', None) for tw in self.tweets_list]
         return lang
 
     def get_tweet_df(self, save=False) -> pd.DataFrame:
->>>>>>> make_unittest
         """required column to be generated you should be creative and add more features"""
 
         columns = ['created_at', 'source', 'original_text', 'polarity', 'subjectivity', 'lang', 'favorite_count', 'retweet_count',
@@ -273,14 +148,8 @@ class TweetDfExtractor:
         hashtags = self.find_hashtags()
         mentions = self.find_mentions()
         location = self.find_location()
-<<<<<<< HEAD
-        # follower_count, friends_count, sensitivity, hashtags, mentions, location)
-        print(type(follower_count), type(friends_count), type(sensitivity))
-        data = zip(created_at, source, text, polarity, subjectivity, lang, fav_count, retweet_count, screen_name, follower_count, friends_count, sensitivity, hashtags, mentions, location)
-=======
         data = zip(created_at, source, text, polarity, subjectivity, lang, fav_count, retweet_count,
                    screen_name, follower_count, friends_count, sensitivity, hashtags, mentions, location)
->>>>>>> make_unittest
         df = pd.DataFrame(data=data, columns=columns)
 
         if save:
@@ -292,17 +161,9 @@ class TweetDfExtractor:
 
 if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
-<<<<<<< HEAD
-    columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
-    'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json("./data/covid19.json")
-    tweet = TweetDfExtractor(tweet_list)
-    tweet_df = tweet.get_tweet_df(True) 
-=======
     columns = ['created_at', 'source', 'original_text', 'clean_text', 'sentiment', 'polarity', 'subjectivity', 'lang', 'favorite_count', 'retweet_count',
                'original_author', 'screen_count', 'followers_count', 'friends_count', 'possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
     _, tweet_list = read_json("./data/covid19.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df(save=True)
     tweet.find_statuses_count()
->>>>>>> make_unittest
