9218e4e7bcd626edee7089189e6651ae8f1490a8
Vincent-bayes <72730186+Vincent-bayes@users.noreply.github.com>
2021-06-26 12:10:56 +0200

    Update clean_tweets_dataframe.py
    code update

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -43,35 +43,3 @@ class Clean_Tweets:
                                                                                              'retweet_count', 'favorite_count'])
 
         return df
-
-    def remove_non_english_tweets(self, df: pd.DataFrame) -> pd.DataFrame:
-        """
-        remove non english tweets from lang
-        """
-        df = df[df["lang"] == "en"]
-#Clean the text
-def clean_tweet(text):
-    text  = "".join([char for char in text if char not in string.punctuation])
-    text = re.sub('[0-9]+', '', text)
-    return text
-
-tweet_df['text'] = tweet_df['text'].apply(lambda x: clean_tweet(x))
-
-def clean_tweet1(source):
-    source  = "".join([char for char in source if char not in string.punctuation])
-    source = re.sub('[0-9]+', '', source)
-    return source
-
-tweet_df['source'] = tweet_df['source'].apply(lambda x: clean_tweet1(x))
-
-# tweet_df.drop['source']
-tweet_df.head()
-df= tweet_df[[ 'fav_count',
-       'follower_count', 'friends_count']]
-plt.subplot(3,1,1)
-plt.plot(df['fav_count'],df['follower_count'])
-plt.subplot(3,1,2)
-plt.plot(df['friends_count'],df['follower_count'])
-plt.subplot(3,1,3)
-plt.plot(df['friends_count'],df['fav_count'])
-plt.show()
