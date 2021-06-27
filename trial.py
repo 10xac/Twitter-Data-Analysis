b4e3c6ac499799ce9452f60fc0e0e696cb9b8617
Vincent-bayes <72730186+Vincent-bayes@users.noreply.github.com>
2021-06-27 23:49:01 +0200

    Add files via upload
    streamlit trial code

---
+++ trial.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+import seaborn as sns
+import requests
+import streamlit as st
+
+print("I am going to be a great Machine Learning Engineer")
+
+st.title("Vinidah Webpage")
