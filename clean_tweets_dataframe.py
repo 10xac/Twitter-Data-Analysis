285632adab744e85d2ed0a69bc4d941721c78671
Ebenezer Olayinka Adeniyi <61415203+Adeben33@users.noreply.github.com>
2022-04-25 16:07:20 +0100

    Add files via upload

---
+++ clean_tweets_dataframe.py
@@ -0,0 +1,56 @@
+class Clean_Tweets:
+    """
+    The PEP8 Standard AMAZING!!!
+    """
+    def __init__(self, df:pd.DataFrame):
+        self.df = df
+        print('Automation in Action...!!!')
+        
+    def drop_unwanted_column(self, df:pd.DataFrame)->pd.DataFrame:
+        """
+        remove rows that has column names. This error originated from
+        the data collection stage.  
+        """
+        unwanted_rows = df[df['retweet_count'] == 'retweet_count' ].index
+        self.df.drop(unwanted_rows , inplace=True)
+        self.df = df[df['polarity'] != 'polarity']
+        
+        return self.df
+    def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
+        """
+        drop duplicate rows
+        """
+        self.df.drop_duplicates(inplace=True)
+        return df
+    def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
+        """
+        convert column to datetime
+        """
+        self.df['created_at'] = pd.to_datetime(self.df['created_at'], errors = 'c')
+        
+        df = df[df['created_at'] >= '2020-12-31' ]
+        
+        return df
+    
+    def convert_to_numbers(self, df:pd.DataFrame)->pd.DataFrame:
+        """
+        convert columns like polarity, subjectivity, retweet_count
+        favorite_count etc to numbers
+        """
+        self.df['polarity'] = pd.to_numeric(df['polarity'])
+        self.df['subjectivity'] = pd.to_numeric(self.df['subjectivity'])
+        self.df['subjectivity'] = pd.to_numeric(self.df['subjectivity'])
+        self.df['retweet_count'] = pd.to_numeric(self.df['retweet_count'])
+        self.df['subjectivity'] = pd.to_numeric(self.df['subjectivity'])
+
+        
+        return df
+    
+    def remove_non_english_tweets(self, df:pd.DataFrame)->pd.DataFrame:
+        """
+        remove non english tweets from lang
+        """
+        
+        self.df = self.df.query("lang = 'en' ", inplace = True)
+        
+        return df
