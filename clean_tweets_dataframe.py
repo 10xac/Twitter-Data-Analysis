0039265de877e9b24ef57ed25fe42fa1e11e2659
stacywairegi <34304187+stacywairegi@users.noreply.github.com>
2022-04-26 17:23:05 +0300

    Update clean_tweets_dataframe

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -2,6 +2,8 @@ class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
     """
+#     import pandas library
+    import pandas as pd
     def __init__(self, df:pd.DataFrame):
         self.df = df
         print('Automation in Action...!!!')
@@ -11,26 +13,26 @@ class Clean_Tweets:
         remove rows that has column names. This error originated from
         the data collection stage.  
         """
+#         fixed bug
         unwanted_rows = df[df['retweet_count'] == 'retweet_count' ].index
         df.drop(unwanted_rows , inplace=True)
-        df = df[df['polarity'] != 'polarity']
+        
         
         return df
     def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
         """
         drop duplicate rows
         """
-        
-        ---
+#    fixed bug     
+        df = df.drop_duplicates(subset-None, keep=False, inplace=True)
         
         return df
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
         """
-        ----
-        
-        ----
+#         fixed bug
+        df['created_at'] = pd.to_datetime(df['created_at'],format = %Y%m%d)
         
         df = df[df['created_at'] >= '2020-12-31' ]
         
@@ -41,10 +43,13 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd.----
-        
-        ----
-        ----
+#         converted to numbers
+        df['polarity'] = pd.to_numeric(df['polarity'])
+        df['subjectivity'] = pd.to_numeric(df['subjectivity'])
+        df['retweet_count'] = pd.to_numeric(df['retweet_count'])
+        df['favourite_count'] = pd.to_numeric(df['favourite_count'])
+        df['followers_count'] = pd.to_numeric(df['followers_count'])
+        df['friends_count'] = pd.to_numeric(df['friends_count'])
         
         return df
     
@@ -52,7 +57,9 @@ class Clean_Tweets:
         """
         remove non english tweets from lang
         """
-        
-        df = ----
+#         check and verify tweet is in english language
+        import ntlk
+        words=set(ntlk.corpus.words.words())
+        df = df['lang.join(r for r in ntlk.wordpunct_tokenize(df['lang'])if r.lower()in words or not r.isalpha())']
         
         return df
