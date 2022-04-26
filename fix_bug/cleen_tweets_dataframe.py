a6a69a3c7dd5ea8c911c1b0f3f1c529868ada189
kebishaa <kbkibatu@gmail.com>
2022-04-26 15:11:46 +0300

    added drop unwanted column

--- fix_bug/cleen_tweets_dataframe.py
+++ fix_bug/cleen_tweets_dataframe.py
@@ -10,4 +10,13 @@ class CleanTweets:
     def __init__(self, df:pd.DataFrame):
         self.df = df
         print('Automation in Action...!!!')
+    def drop_unwanted_column(self)->pd.DataFrame:
+        """
+        remove rows that has column names. This error originated from
+        the data collection stage.  
+        """
+        unwanted_rows = self.df[self.df['retweet_count'] == 'retweet_count' ].index
+        self.df.drop(unwanted_rows , inplace=True)
+        self.df = self.df[self.df['polarity'] != 'polarity']
         
+        return self.df
