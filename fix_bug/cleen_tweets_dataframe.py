d639f99aa6d2d7a3a3acc206499406cae5e138ab
kebishaa <kbkibatu@gmail.com>
2022-04-26 15:07:40 +0300

    created a class

---
+++ fix_bug/cleen_tweets_dataframe.py
@@ -0,0 +1,13 @@
+
+class CleanTweets:
+    """
+    This class is responsible for cleaning the twitter dataframe
+
+    Returns:
+    --------
+    A dataframe
+    """
+    def __init__(self, df:pd.DataFrame):
+        self.df = df
+        print('Automation in Action...!!!')
+        
