ce062eb017dcb98b6e00eca40cc7c408494fc9d5
fbagire <faibagire@gmail.com>
2022-04-26 14:37:48 +0200

    Added place_coord_boundaries column

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -162,6 +162,10 @@ class TweetDfExtractor:
 
         return location
 
+    def get_coordinates(self) -> list:
+        coordinates = [x['coordinates'] for x in self.tweets_list]
+        return coordinates
+
     def get_tweet_df(self, save=False) -> pd.DataFrame:
         # save = True
         """required column to be generated you should be creative and add more features"""
@@ -172,14 +176,13 @@ class TweetDfExtractor:
                    'original_author', 'followers_count', 'friends_count', 'possibly_sensitive', 'hashtags',
                    'user_mentions', 'place']
 
-        # columns to add = ['sentiment','screen_count','place_coord_boundaries']
+        # columns to add = ['sentiment','screen_count']
 
         created_at = self.find_created_time()
         source = self.find_source()
         text = self.find_full_text()
         text_new = self.text_cleaner(text)
         polarity, subjectivity = self.find_sentiments(text)
-        polarity_clean, subjectivity_clean = self.find_sentiments(text_new)
         lang = self.find_lang()
         fav_count = self.find_favourite_count()
         retweet_count = self.find_retweet_count()
@@ -190,13 +193,13 @@ class TweetDfExtractor:
         hashtags = self.find_hashtags()
         mentions = self.find_mentions()
         location = self.find_location()
+        coordinates = self.get_coordinates()
 
         data_dic = {'created_at': created_at, 'source': source, 'original_text': text, 'cleaned_text': text_new,
-                    'polarity': polarity, 'polarity_clean': polarity_clean, 'subjectivity_clean': subjectivity_clean,
-                    'subjectivity': subjectivity, 'lang': lang, 'favorite_count': fav_count,
+                    'polarity': polarity, 'subjectivity': subjectivity, 'lang': lang, 'favorite_count': fav_count,
                     'retweet_count': retweet_count, 'original_author': screen_name, 'followers_count': follower_count,
                     'friends_count': friends_count, 'possibly_sensitive': sensitivity, 'hashtags': hashtags,
-                    'user_mentions': mentions, 'place': location}
+                    'user_mentions': mentions, 'place': location, 'place_coord_boundaries': coordinates}
         df = pd.DataFrame(data=data_dic)
 
         if save:
