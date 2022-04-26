8473481d6b8810728a6b455e4899e78e0ce4819d
nahomHmichael <nahomhabtemichael@gmail.com>
2022-04-26 19:30:16 +0300

    fixed more blank funcs in extract_dataframe.py

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -47,27 +47,47 @@ class TweetDfExtractor:
         return statuses_count
 
     def find_full_text(self) -> list:
-        pass
-        # text =
 
-    def find_sentiments(self, text) -> list:
-        pass
+        text =[]
+        for tweet in self.tweets_list:
 
-        # return polarity, self.subjectivity
+            if 'retweeted_status' in tweet.keys() and \
+                    'extended_tweet' in tweet['retweeted_status'].keys():
+                text.append(tweet['retweeted_status']
+                            ['extended_tweet']['full_text'])
+            else:
+                text.append('Empty')
+        return text
+
+    def find_sentiments(self, text: list) -> list:
+        #pass
+        polarity, subjectivity = [], []
+        for tweet in text:
+            blob = TextBlob(tweet)
+            sentiment = blob.sentiment
+            polarity.append(sentiment.polarity)
+            subjectivity.append(sentiment.subjectivity)
+
+        return polarity, subjectivity
 
     def find_created_time(self) -> list:
-        pass
-        # return created_at
+        #pass
+        created_at = []
+        for t in self.tweets_list:
+            created_at.append(t['created_at'])
+        return created_at
+
 
     def find_source(self) -> list:
-        pass
-        # source =
+        #pass
+        source = [x['source'] for x in self.tweets_list]
 
-        # return source
-
+        return source
     def find_screen_name(self) -> list:
-        pass
-        # screen_name =
+        #pass
+        screen_name = [x['user']['screen_name'] for x in self.tweets_list]
+
+        return screen_name
 
     def find_followers_count(self) -> list:
         pass
@@ -118,21 +138,22 @@ class TweetDfExtractor:
                    'original_author', 'followers_count', 'friends_count', 'possibly_sensitive', 'hashtags',
                    'user_mentions', 'place']
 
-        # created_at = self.find_created_time()
-        # source = self.find_source()
+        created_at = self.find_created_time()
+        source = self.find_source()
         text = self.find_full_text()
-        # polarity, subjectivity = self.find_sentiments(text)
-        # lang = self.find_lang()
-        # fav_count = self.find_favourite_count()
-        # retweet_count = self.find_retweet_count()
-        # screen_name = self.find_screen_name()
-        # follower_count = self.find_followers_count()
-        # friends_count = self.find_friends_count()
-        # sensitivity = self.is_sensitive()
-        # hashtags = self.find_hashtags()
-        # mentions = self.find_mentions()
-        # location = self.find_location()
-        data = zip(created_at, source, text, polarity, subjectivity, lang, fav_count, retweet_count, screen_name, follower_count, friends_count, sensitivity, hashtags, mentions, location)
+        polarity, subjectivity = self.find_sentiments(text)
+        lang = self.find_lang()
+        fav_count = self.find_favourite_count()
+        retweet_count = self.find_retweet_count()
+        screen_name = self.find_screen_name()
+        follower_count = self.find_followers_count()
+        friends_count = self.find_friends_count()
+        sensitivity = self.is_sensitive()
+        hashtags = self.find_hashtags()
+        mentions = self.find_mentions()
+        location = self.find_location()
+        data = zip(created_at, source, text, polarity, subjectivity, lang, fav_count, retweet_count, screen_name,
+                   follower_count, friends_count, sensitivity, hashtags, mentions, location)
         df = pd.DataFrame(data=data, columns=columns)
 
         if save:
