713afc6bfe1bfa551d169541a427c9fb993ae9a4
d1m3j1 <dimejishekoni@gmail.com>
2022-05-02 14:24:34 +0000

    first pipeline attempt

---
+++ tweeter_pipeline/schema.sql
@@ -0,0 +1,25 @@
+
+
+CREATE TABLE IF NOT EXISTS `TweetInformation` 
+(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `statuses_count` INT DEFAULT NULL, 
+    `created_at` TEXT NOT NULL,
+    `source` VARCHAR(200) NOT NULL,
+    `text` TEXT DEFAULT NULL,
+    `polarity` FLOAT DEFAULT NULL,
+    `subjectivity` FLOAT DEFAULT NULL,
+    `lang` TEXT DEFAULT NULL,
+    `favorite_count` INT DEFAULT NULL,
+    `retweet_count` INT DEFAULT NULL,
+    `screen_name` TEXT DEFAULT NULL,
+    `followers_count` INT DEFAULT NULL,
+    `friends_count` INT DEFAULT NULL,
+    `sensitivity` FLOAT DEFAULT NULL,
+    `hashtags` TEXT DEFAULT NULL,
+    `user_mentions` TEXT DEFAULT NULL,
+    `place` TEXT DEFAULT NULL,
+
+    PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
